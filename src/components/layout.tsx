24c3452b934d2aa8027312a6243eccc261dba2c6
Mark Taylor <mtaylor@Marks-MacBook-Pro.local>
2022-02-10 10:35:00 +0000

    PRACMIG-625: Reformat files
    Reformat files according to prettier config.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,7 +7,7 @@
 
 import * as React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import {useStaticQuery, graphql} from "gatsby"
 
 import Header from "./header"
 import "./layout.scss"
@@ -17,8 +17,8 @@ interface LayoutProps {
     children: ReactNode;
 }
 
-const Layout : FC<LayoutProps> = ({ children }) => {
-  const data = useStaticQuery(graphql`
+const Layout: FC<LayoutProps> = ({children}) => {
+    const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
         siteMetadata {
@@ -28,33 +28,37 @@ const Layout : FC<LayoutProps> = ({ children }) => {
     }
   `)
 
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <Header siteTitle={data.site.siteMetadata?.title || `Title`}/>
+            <div
+                style={{
+                    margin: `0 auto`,
+                    maxWidth: 960,
+                    padding: `0 1.0875rem 1.45rem`,
+                }}
+            >
+                <main className="nhsuk-main-wrapper app-homepage" id="maincontent">
+                    <section className="app-homepage-content">
+                        <div className="nhsuk-width-container">{children}</div>
+                    </section>
+                </main>
+                <footer
+                    style={{
+                        marginTop: `2rem`,
+                    }}
+                >
+                    © {new Date().getFullYear()}, Built with
+                    {` `}
+                    <a href="https://www.gatsbyjs.com">Gatsby</a>
+                </footer>
+            </div>
+        </>
+    )
 }
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+    children: PropTypes.node.isRequired,
 }
 
 export default Layout
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,33 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import {Link} from "gatsby"
+import {StaticImage} from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import {FC} from "react";
 import Seo from "../components/seo";
 
 const IndexPage: FC = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
-  </Layout>
+    <Layout>
+        <Seo title="Home"/>
+        <div className="nhsuk-u-reading-width">
+            <h2>{"Quick test"}</h2>
+            <p>{"Something interesting"}</p>
+        </div>
+        <StaticImage
+            src="../images/gatsby-astronaut.png"
+            width={300}
+            quality={95}
+            formats={["auto", "webp", "avif"]}
+            alt="A Gatsby astronaut"
+            style={{marginBottom: `1.45rem`}}
+        />
+        <p>
+            <Link to="/page-2/">Go to page 2</Link> <br/>
+            <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br/>
+            <Link to="/using-ssr">Go to "Using SSR"</Link> <br/>
+            <Link to="/using-dsg">Go to "Using DSG"</Link>
+        </p>
+    </Layout>
 )
 
 export default IndexPage
