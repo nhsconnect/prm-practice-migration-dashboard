20561e55629a2309c2d0f9e875ed4e6f307002c5
Mark Taylor <mtaylor@Marks-MacBook-Pro.local>
2022-02-10 15:08:55 +0000

    PRACMIG-625: Add empty table to page
    Add an empty table (just has headers) to the main page.

--- src/pages/__tests__/index.test.tsx
+++ src/pages/__tests__/index.test.tsx
@@ -1,14 +1,15 @@
-import * as React from "react"
+import * as React from "react";
 
-import {render} from "@testing-library/react";
-import IndexPage from '../index';
+import { render, screen } from "@testing-library/react";
+import IndexPage from "../index";
 
-it('renders a table', () => {
-    render(<IndexPage />);
+it("renders a table", () => {
+  render(<IndexPage />);
 
-    // expect(screen.queryByText("Date")).toBeTruthy();
-    // expect(screen.queryByText("Practice Name")).toBeTruthy();
-    // expect(screen.queryByText("Source Supplier")).toBeTruthy();
-    // expect(screen.queryByText("Target Supplier")).toBeTruthy();
-    // expect(screen.queryByText("Cutover Duration (Days)")).toBeTruthy();
+  expect(screen.queryByText("Cutover Start Date")).toBeTruthy();
+  expect(screen.queryByText("Cutover End Date")).toBeTruthy();
+  expect(screen.queryByText("Practice Name")).toBeTruthy();
+  expect(screen.queryByText("Source System")).toBeTruthy();
+  expect(screen.queryByText("Target System")).toBeTruthy();
+  expect(screen.queryByText("Cutover Duration (Days)")).toBeTruthy();
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,24 @@
-import * as React from "react"
+import { Table } from "nhsuk-react-components";
+import * as React from "react";
 
-import {FC} from "react";
+import { FC } from "react";
 import Layout from "../components/layout";
 
-const IndexPage: FC = () => (<Layout>
+const IndexPage: FC = () => (
+  <Layout>
+    <Table>
+      <Table.Head>
+        <Table.Row>
+          <Table.Cell>Cutover Start Date</Table.Cell>
+          <Table.Cell>Cutover End Date</Table.Cell>
+          <Table.Cell>Practice Name</Table.Cell>
+          <Table.Cell>Source System</Table.Cell>
+          <Table.Cell>Target System</Table.Cell>
+          <Table.Cell>Cutover Duration (Days)</Table.Cell>
+        </Table.Row>
+      </Table.Head>
+    </Table>
+  </Layout>
+);
 
-</Layout>)
-
-export default IndexPage
+export default IndexPage;
