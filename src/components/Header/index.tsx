d83c101d8a62a14a794a07d1644cfad08b6dfec1
Caius Eugene <caius.eugene1@nhs.net>
2022-02-15 14:26:54 +0000

    PRACMIG-625: Use official NHSUK header component
    - https://nhsdigital.github.io/nhsuk-react-components/?path=/docs/components-header--transactional-header-with-service-name
    
    Caius

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,30 +1,20 @@
 import React from "react";
-import { Link } from "gatsby";
-import Logo from "../../assets/logo.svg";
+// import { Link } from "gatsby";
+// import Logo from "../../assets/logo.svg";
 import content from "../../data/content/header.json";
 
 import { FC } from "react";
+import {Header} from "nhsuk-react-components";
 
 interface HeaderProps {}
 
-export const Header: FC<HeaderProps> = () => (
-  <header className="nhsuk-header" role="banner">
-    <div className="nhsuk-width-container nhsuk-header__container">
-      <div className="nhsuk-header__logo nhsuk-header__logo--only">
-        <Link
-          className="gp2gp-header__link"
-          to="/"
-          aria-label={content.homepageLinkLabel}
-        >
-          <Logo />
-          <span className="nhsuk-header__service-name">
-            {content.serviceName}
-          </span>
-        </Link>
-      </div>
-    </div>
-    <div className="nhsuk-width-container nhsuk-u-padding-bottom-3"></div>
-  </header>
+export const HeaderContainer: FC<HeaderProps> = () => (
+    <Header transactional>
+        <Header.Container>
+            <Header.Logo href="/" />
+            <Header.ServiceName href="/">{content.serviceName}</Header.ServiceName>
+        </Header.Container>
+    </Header>
 );
 
-export default Header;
+export default HeaderContainer;
