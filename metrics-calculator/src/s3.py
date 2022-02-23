c57443f0795e7466a47083300a12f026ee347ba1
Mark Taylor <86659797+marktaylor80@users.noreply.github.com>
2022-02-23 10:28:23 +0000

    PRACMIG-626: Test reading csv data
    Caius / Mark

--- metrics-calculator/src/s3.py
+++ metrics-calculator/src/s3.py
@@ -2,16 +2,24 @@ import csv
 import gzip
 import sys
 import logging
+from urllib.parse import urlparse
 
 logger = logging.getLogger(__name__)
 
 
-def read_gzip_csv(self, object_uri: str):
+def _object_from_uri(client, uri: str):
+    object_url = urlparse(uri)
+    s3_bucket = object_url.netloc
+    s3_key = object_url.path.lstrip("/")
+    return client.Object(s3_bucket, s3_key)
+
+
+def read_gzip_csv(client, object_uri: str):
     logger.info(
         "Reading file from: " + object_uri,
         extra={"event": "READING_FILE_FROM_S3", "object_uri": object_uri},
-        )
-    s3_object = self._object_from_uri(object_uri)
+    )
+    s3_object = _object_from_uri(client, object_uri)
 
     try:
         response = s3_object.get()
@@ -19,7 +27,7 @@ def read_gzip_csv(self, object_uri: str):
         with gzip.open(body, mode="rt") as f:
             input_csv = csv.DictReader(f)
             yield from input_csv
-    except self._client.meta.client.exceptions.NoSuchKey:
+    except client.meta.client.exceptions.NoSuchKey:
         logger.error(
             f"File not found: {object_uri}, exiting...",
             extra={"event": "FILE_NOT_FOUND_IN_S3", "object_uri": object_uri},
