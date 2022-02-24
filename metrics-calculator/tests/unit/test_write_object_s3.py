7317d449096c8a617fca377ec1bfddf9b25b1f13
Caius Eugene <caius.eugene1@nhs.net>
2022-02-24 13:17:44 +0000

    PRACMIG-626: Func to write object to s3

---
+++ metrics-calculator/tests/unit/test_write_object_s3.py
@@ -0,0 +1,16 @@
+import boto3
+from moto import mock_s3
+
+from src.read_object_s3 import write_object_s3
+
+
+@mock_s3
+def test_writes_object_content():
+    s3 = boto3.resource("s3", region_name="us-east-1")
+    s3.create_bucket(Bucket="test_bucket")
+
+    json_string = b'{"fruit": "mango"}'
+    write_object_s3(s3, "s3://test_bucket/test_object.json", json_string)
+
+    s3_object_response = s3.Object("test_bucket", "test_object.json").get()
+    assert s3_object_response["Body"].read() == json_string
