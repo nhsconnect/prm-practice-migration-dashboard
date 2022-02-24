2b3f41f6fdf765accd51ddf2eb69ccb646e0e956
Mark Taylor <86659797+marktaylor80@users.noreply.github.com>
2022-02-24 14:25:13 +0000

    PRACMIG-626: Move test
    Move the test for the read_object_s3 function into the test_s3 file so
     that the test file structure matches the src code structure.

--- metrics-calculator/tests/unit/test_s3.py
+++ metrics-calculator/tests/unit/test_s3.py
@@ -1,7 +1,34 @@
 import boto3
+import gzip
 from moto import mock_s3
 
+from src.s3 import read_object_s3
 from src.s3 import write_object_s3
+from tests.builders.file import build_gzip_csv
+
+
+@mock_s3
+def test_read_object_s3_returns_object_content():
+    s3 = boto3.resource("s3", region_name="us-east-1")
+    bucket = s3.create_bucket(Bucket="test_bucket")
+    s3_object = bucket.Object("test_object.csv.gz")
+    gzipped_content = build_gzip_csv(
+        header=["id", "message", "comment"],
+        rows=[["123", "A message", "A comment"], [
+            "321", "Another message", "Another comment"]],
+    )
+    s3_object.put(
+        Body=gzipped_content
+    )
+
+    expected = "id,message,comment\n123,A message,A comment\n321,Another message,Another comment"
+
+    csv_stream = read_object_s3(s3, "s3://test_bucket/test_object.csv.gz")
+
+    with gzip.open(csv_stream, mode="rt") as f:
+        actual = f.read()
+
+    assert actual == expected
 
 
 @mock_s3
