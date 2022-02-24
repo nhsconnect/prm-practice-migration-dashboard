4ef4b45c85a7b240ea5dafae58ff208e68f3536e
Mark Taylor <86659797+marktaylor80@users.noreply.github.com>
2022-02-24 11:45:51 +0000

    PRACMIG-626: Remove commented-out code

--- metrics-calculator/src/app.py
+++ metrics-calculator/src/app.py
@@ -47,24 +47,3 @@ def get_org_details(ods_code):
         "source_system": "Ye Olde System",
         "target_system": "Shiny NU",
     }
-
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
