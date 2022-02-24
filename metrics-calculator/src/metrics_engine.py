8fcd2d5d797441fca77d447c5b4248977a928f64
Caius Eugene <caius.eugene1@nhs.net>
2022-02-24 10:37:07 +0000

    PRACMIG-626: Calculate cutover start, end and duration
    - Single migration based on 2 splunk log extracts ( 1 per ASID )
    
    Mark / Caius

---
+++ metrics-calculator/src/metrics_engine.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+
+from dateutil.parser import *
+import pytz
+import functools
+
+
+def get_oldest_message_date(ref_date, message):
+    current_date = isoparse(message["_time"])
+    result_date = ref_date
+    if current_date < ref_date:
+        result_date = current_date
+    return result_date
+
+
+def get_newest_message_date(ref_date, message):
+    current_date = isoparse(message["_time"])
+    result_date = ref_date
+    if current_date > ref_date:
+        result_date = current_date
+    return result_date
+
+
+def calculate_cutover_start_and_end_date(old_asid_extract_generator, new_asid_extract_generator):
+    old_asid_newest_message_date = functools.reduce(get_newest_message_date, old_asid_extract_generator, pytz.utc.localize(datetime.min))
+    new_asid_oldest_message_date = functools.reduce(get_oldest_message_date, new_asid_extract_generator, pytz.utc.localize(datetime.max))
+
+    cutover_duration = new_asid_oldest_message_date - old_asid_newest_message_date
+
+    return {
+        "cutover_startdate": old_asid_newest_message_date.isoformat(),
+        "cutover_enddate": new_asid_oldest_message_date.isoformat(),
+        "cutover_duration": cutover_duration.days
+    }
+
+
+
+
---
+++ metrics-calculator/tests/unit/test_metrics_engine.py
@@ -0,0 +1,22 @@
+from src.metrics_engine import calculate_cutover_start_and_end_date
+
+
+def test_calculate_cutover_start_and_end_date():
+    old_asid_extract_generator = (x for x in [
+        {"_time": "2021-01-01T13:23:26.649+0000"},
+        {"_time": "2021-01-02T13:23:26.649+0000"},
+        {"_time": "2021-01-03T13:23:26.649+0000"}
+    ])
+    new_asid_extract_generator = (x for x in [
+        {"_time": "2021-01-10T13:23:26.649+0000"},
+        {"_time": "2021-01-11T13:23:26.649+0000"},
+        {"_time": "2021-01-12T13:23:26.649+0000"}
+    ])
+
+    result = calculate_cutover_start_and_end_date(old_asid_extract_generator, new_asid_extract_generator)
+
+    assert result == {
+        "cutover_startdate": "2021-01-03T13:23:26.649000+00:00",
+        "cutover_enddate": "2021-01-10T13:23:26.649000+00:00",
+        "cutover_duration": 7
+    }
