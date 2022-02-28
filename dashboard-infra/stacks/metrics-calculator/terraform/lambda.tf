5619786f411bd89bd3b2a63498655c2bff7f2857
Caius Eugene <caius.eugene1@nhs.net>
2022-02-28 15:07:57 +0000

    PRACMIG-626: Lambda resource added to metrics-calculator tf stack

---
+++ dashboard-infra/stacks/metrics-calculator/terraform/lambda.tf
@@ -0,0 +1,37 @@
+resource "aws_lambda_function" "metric_calculator_function" {
+  function_name = var.metrics_calculator_function_name
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = var.metrics_calculator_handler_name
+
+  runtime       = "python3.9"
+
+  s3_bucket     = var.metrics_calculator_deployment_bucket_name
+  s3_key        = var.metrics_calculator_code_key
+
+  environment {
+    variables = {
+      TELEMETRY_BUCKET_NAME = var.telemetry_bucket_name
+      METRICS_BUCKET_NAME   = var.metrics_bucket_name
+    }
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
--- dashboard-infra/stacks/metrics-calculator/terraform/variables.tf
+++ dashboard-infra/stacks/metrics-calculator/terraform/variables.tf
@@ -26,3 +26,28 @@ variable "telemetry_bucket_name" {
   type        = string
   description = "Name of bucket which telemetry data is retrieved from"
 }
+
+variable "metrics_bucket_name" {
+  type        = string
+  description = "Name of bucket which metrics data is written"
+}
+
+variable "metrics_calculator_function_name" {
+  type    = string
+  default = "metric_calculator"
+}
+
+variable "metrics_calculator_deployment_bucket_name" {
+  type    = string
+  description = "Name of bucket where the metrics calculator package is deployed"
+}
+
+variable "metrics_calculator_code_key" {
+  type    = string
+  description = "Object key for metrics calculator deployed code"
+}
+
+variable "metrics_calculator_handler_name" {
+  type    = string
+  description = "Handler function name for the metrics calculator"
+}
