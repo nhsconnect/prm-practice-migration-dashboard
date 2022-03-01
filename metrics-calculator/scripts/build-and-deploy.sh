755b82782515084f26f5324c83941d9778da9dbe
Mark Taylor <86659797+marktaylor80@users.noreply.github.com>
2022-03-01 11:29:59 +0000

    PRACMIG-626: Refactor build script
    - Create a variable for the entire S3 bucket name.
    - Remove the "S3" prefix from variable names (added unnecessary noise).
    - Surround bash variables with braces to remove ambiguity.
    - Make long AWS CLI commands multiline to improve readability.

--- metrics-calculator/scripts/build-and-deploy.sh
+++ metrics-calculator/scripts/build-and-deploy.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
-S3_BUCKET=metric-calculator-deployments
+BUCKET_ROOT_NAME=metric-calculator-deployments
 ENV=dev
+BUCKET_NAME="${BUCKET_ROOT_NAME}-${ENV}"
 
 chalice package build
 
-if aws s3api head-bucket --bucket "$S3_BUCKET-$ENV" 2>&1 | grep -q 'Not Found'
+if aws s3api head-bucket --bucket "${BUCKET_NAME}" 2>&1 | grep -q 'Not Found'
 then
-  aws s3api create-bucket --bucket "$S3_BUCKET-$ENV" --acl private --create-bucket-configuration '{ "LocationConstraint": "eu-west-2" }'
-  aws s3api wait bucket-exists --bucket "$S3_BUCKET-$ENV"
-  aws s3api put-public-access-block --bucket "$S3_BUCKET-$ENV" --public-access-block-configuration BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true
+  aws s3api create-bucket \
+    --bucket "${BUCKET_NAME}" \
+    --acl private \
+    --create-bucket-configuration '{ "LocationConstraint": "eu-west-2" }'
+  aws s3api wait bucket-exists --bucket "${BUCKET_NAME}"
+  aws s3api put-public-access-block \
+    --bucket "${BUCKET_NAME}" \
+    --public-access-block-configuration BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true
 fi
 
-aws cloudformation package --template-file build/sam.json --s3-bucket "$S3_BUCKET-$ENV" --output-template-file "./build/packaged-cf.json" --use-json
-aws cloudformation deploy --template-file "./build/packaged-cf.json" --stack-name "metrics-calculator-$ENV" --capabilities CAPABILITY_IAM
+aws cloudformation package \
+  --template-file build/sam.json \
+  --s3-bucket "${BUCKET_NAME}" \
+  --output-template-file "./build/packaged-cf.json" \
+  --use-json
+aws cloudformation deploy \
+  --template-file "./build/packaged-cf.json" \
+  --stack-name "metrics-calculator-${ENV}" \
+  --capabilities CAPABILITY_IAM
 
 # Outputs to use in tfvars on the metrics calculator stack
 JSON=$(cat "./build/packaged-cf.json")
-echo $JSON | jq .Resources.CalculateDashboardMetricsFromTelemetry.Properties.Handler
-echo $JSON | jq .Resources.CalculateDashboardMetricsFromTelemetry.Properties.CodeUri
+echo "${JSON}" | jq .Resources.CalculateDashboardMetricsFromTelemetry.Properties.Handler
+echo "${JSON}" | jq .Resources.CalculateDashboardMetricsFromTelemetry.Properties.CodeUri
