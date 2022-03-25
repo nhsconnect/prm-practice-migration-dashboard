40624d51b9960ec211e97df64b8e2ffdc3f0764b
Mark Taylor <86659797+marktaylor80@users.noreply.github.com>
2022-03-25 11:32:49 +0000

    PRACMIG-668: Import module with matching case
    *Debugging failing CI pipeline*
    
    Import the header module with the same case as the module's directory
     name.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,6 @@
 import React, { FC, ReactNode } from "react";
 
-import Header from "./header";
+import Header from "./Header";
 import "./layout.scss";
 import { Footer, Hero } from "nhsuk-react-components";
 import { PhaseBanner } from "./PhaseBanner";
