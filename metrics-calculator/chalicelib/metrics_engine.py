990fdf09a7ac27561308bee255ac514a720a1e8e
Caius Eugene <caius.eugene1@nhs.net>
2022-04-11 16:12:48 +0100

    PRACMIG-687: Light refactor of metrics engine
    Sama / Caius

--- metrics-calculator/chalicelib/metrics_engine.py
+++ metrics-calculator/chalicelib/metrics_engine.py
@@ -11,12 +11,13 @@ def find_day_before_normal_activity(activity_per_day, threshold):
         day_before_normal_activity_date = isoparse(day["_time"])
 
 
-def calculate_cutover_start_and_end_date(
-        old_asid_extract_generator, new_asid_extract_generator):
-    first_day = next(itertools.islice(old_asid_extract_generator, 1))
+def calculate_cutover_start_and_end_date(old_asid_extract_generator, new_asid_extract_generator):
+    old_asid_activity_per_day = list(old_asid_extract_generator)
+    new_asid_activity_per_day = list(new_asid_extract_generator)
+    first_day = old_asid_activity_per_day[0]
     threshold = float(first_day["avgmin2std"])
-    old_asid_newest_message_date = find_day_before_normal_activity(reversed(list(old_asid_extract_generator)), threshold)
-    new_asid_oldest_message_date = find_day_before_normal_activity(list(new_asid_extract_generator), threshold)
+    old_asid_newest_message_date = find_day_before_normal_activity(reversed(old_asid_activity_per_day), threshold)
+    new_asid_oldest_message_date = find_day_before_normal_activity(new_asid_activity_per_day, threshold)
 
     cutover_duration = new_asid_oldest_message_date - old_asid_newest_message_date
 
