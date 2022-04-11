2cd7b279bca69ee09c8620e29b276ea8b9308c1c
Caius Eugene <caius.eugene1@nhs.net>
2022-04-11 15:21:37 +0100

    PRACMIG-687: Calculate cutover dates using Normal Activity
    - Refactored intergration test to use new telemetry data format
    - Refactored metrics calculator
    
    Sama / Caius

--- metrics-calculator/chalicelib/metrics_engine.py
+++ metrics-calculator/chalicelib/metrics_engine.py
@@ -1,36 +1,31 @@
+import itertools
 from datetime import datetime
 
 from dateutil.parser import *
 import pytz
 import functools
 
+def first_message_below_threshold(old_asid_extract_generator, threshold):
+    reversed_extract_dates_list = reversed(list(old_asid_extract_generator))
+    first_message_date = None
+    for day in reversed_extract_dates_list:
+        if float(day["count"]) > threshold:
+            return first_message_date
+        first_message_date = isoparse(day["_time"])
 
-def get_oldest_message_date(ref_date, message):
-    current_date = isoparse(message["_time"])
-    result_date = ref_date
-    if current_date < ref_date:
-        result_date = current_date
-    return result_date
-
-
-def get_newest_message_date(ref_date, message):
-    current_date = isoparse(message["_time"])
-    result_date = ref_date
-    if current_date > ref_date:
-        result_date = current_date
-    return result_date
-
+def last_message_below_threshold(new_asid_extract_generator, threshold):
+    last_message_date = None
+    for day in new_asid_extract_generator:
+        if float(day["count"]) > threshold:
+            return last_message_date
+        last_message_date = isoparse(day["_time"])
 
 def calculate_cutover_start_and_end_date(
         old_asid_extract_generator, new_asid_extract_generator):
-    old_asid_newest_message_date = functools.reduce(
-        get_newest_message_date,
-        old_asid_extract_generator,
-        pytz.utc.localize(datetime.min))
-    new_asid_oldest_message_date = functools.reduce(
-        get_oldest_message_date,
-        new_asid_extract_generator,
-        pytz.utc.localize(datetime.max))
+    first_day = next(itertools.islice(old_asid_extract_generator, 1))
+    threshold = float(first_day["avgmin2std"])
+    old_asid_newest_message_date = first_message_below_threshold(old_asid_extract_generator, threshold)
+    new_asid_oldest_message_date = last_message_below_threshold(new_asid_extract_generator, threshold)
 
     cutover_duration = new_asid_oldest_message_date - old_asid_newest_message_date
 
--- metrics-calculator/tests/unit/test_metrics_engine.py
+++ metrics-calculator/tests/unit/test_metrics_engine.py
@@ -3,21 +3,24 @@ from chalicelib.metrics_engine import calculate_cutover_start_and_end_date
 
 def test_calculate_cutover_start_and_end_date():
     old_asid_extract_generator = (x for x in [
-        {"_time": "2021-01-01T13:23:26.649+0000"},
-        {"_time": "2021-01-02T13:23:26.649+0000"},
-        {"_time": "2021-01-03T13:23:26.649+0000"}
-    ])
+        {"_time":"2021-11-25T00:00:00.000+0000","count": "2000","avgmin2std": "1044.7268404372467"},
+        {"_time":"2021-11-26T00:00:00.000+0000","count": "2854","avgmin2std": "1044.7268404372467"},
+        {"_time":"2021-11-27T00:00:00.000+0000","count": "2754","avgmin2std": "1044.7268404372467"},
+        {"_time":"2021-12-02T15:42:00.000+0000","count": "200","avgmin2std": "1044.7268404372467"},
+        {"_time":"2021-12-03T15:42:00.000+0000","count": "200","avgmin2std": "1044.7268404372467"},
+        {"_time":"2021-12-04T15:42:00.000+0000","count": "200","avgmin2std": "1044.7268404372467"}])
     new_asid_extract_generator = (x for x in [
-        {"_time": "2021-01-10T13:23:26.649+0000"},
-        {"_time": "2021-01-11T13:23:26.649+0000"},
-        {"_time": "2021-01-12T13:23:26.649+0000"}
+        {"_time":"2021-12-05T15:42:00.000+0000","count":"300","avgmin2std":"1044.7268404372467"},
+        {"_time":"2021-12-06T00:00:00.000+0000","count":"2854","avgmin2std":"1044.7268404372467"},
+        {"_time":"2021-12-07T00:00:00.000+0000","count":"2754","avgmin2std":"1044.7268404372467"},
+        {"_time":"2021-12-08T00:00:00.000+0000","count":"2554","avgmin2std":"1044.7268404372467"}
     ])
 
     result = calculate_cutover_start_and_end_date(
         old_asid_extract_generator, new_asid_extract_generator)
 
     assert result == {
-        "cutover_startdate": "2021-01-03T13:23:26.649000+00:00",
-        "cutover_enddate": "2021-01-10T13:23:26.649000+00:00",
-        "cutover_duration": 7
+        "cutover_startdate": "2021-12-02T15:42:00+00:00",
+        "cutover_enddate": "2021-12-05T15:42:00+00:00",
+        "cutover_duration": 3,
     }
