cfbfbf2da5027e09b6f90c9f23e9b096f803c443
Caius Eugene <caius.eugene1@nhs.net>
2022-04-11 16:00:56 +0100

    PRACMIG-687: DRYed up the metrics-engine

--- metrics-calculator/chalicelib/metrics_engine.py
+++ metrics-calculator/chalicelib/metrics_engine.py
@@ -2,27 +2,21 @@ import itertools
 
 from dateutil.parser import *
 
-def first_message_below_threshold(old_asid_extract_generator, threshold):
-    reversed_extract_dates_list = reversed(list(old_asid_extract_generator))
-    first_message_date = None
-    for day in reversed_extract_dates_list:
-        if float(day["count"]) > threshold:
-            return first_message_date
-        first_message_date = isoparse(day["_time"])
 
-def last_message_below_threshold(new_asid_extract_generator, threshold):
-    last_message_date = None
-    for day in new_asid_extract_generator:
+def find_day_before_normal_activity(activity_per_day, threshold):
+    day_before_normal_activity_date = None
+    for day in activity_per_day:
         if float(day["count"]) > threshold:
-            return last_message_date
-        last_message_date = isoparse(day["_time"])
+            return day_before_normal_activity_date
+        day_before_normal_activity_date = isoparse(day["_time"])
+
 
 def calculate_cutover_start_and_end_date(
         old_asid_extract_generator, new_asid_extract_generator):
     first_day = next(itertools.islice(old_asid_extract_generator, 1))
     threshold = float(first_day["avgmin2std"])
-    old_asid_newest_message_date = first_message_below_threshold(old_asid_extract_generator, threshold)
-    new_asid_oldest_message_date = last_message_below_threshold(new_asid_extract_generator, threshold)
+    old_asid_newest_message_date = find_day_before_normal_activity(reversed(list(old_asid_extract_generator)), threshold)
+    new_asid_oldest_message_date = find_day_before_normal_activity(list(new_asid_extract_generator), threshold)
 
     cutover_duration = new_asid_oldest_message_date - old_asid_newest_message_date
 
