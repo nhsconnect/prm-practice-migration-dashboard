c93d97cde92ac11d891cc2f7216450bbf4691d92
Caius Eugene <caius.eugene1@nhs.net>
2022-04-13 09:52:01 +0100

    PRACMIG-687: Refactored metrics method for finding last entry below threshold
    Mark / Sama / Caius

--- metrics-calculator/chalicelib/metrics_engine.py
+++ metrics-calculator/chalicelib/metrics_engine.py
@@ -3,12 +3,12 @@ import itertools
 from dateutil.parser import *
 
 
-def find_day_before_normal_activity(activity_per_day, threshold):
-    day_before_normal_activity_date = None
+def find_last_entry_below_threshold(activity_per_day, threshold):
+    date_of_last_entry = None
     for day in activity_per_day:
         if float(day["count"]) > threshold:
-            return day_before_normal_activity_date
-        day_before_normal_activity_date = isoparse(day["_time"])
+            return date_of_last_entry
+        date_of_last_entry = isoparse(day["_time"])
 
 
 def calculate_cutover_start_and_end_date(old_asid_extract_generator, new_asid_extract_generator):
@@ -16,8 +16,8 @@ def calculate_cutover_start_and_end_date(old_asid_extract_generator, new_asid_ex
     new_asid_activity_per_day = list(new_asid_extract_generator)
     first_day = old_asid_activity_per_day[0]
     threshold = float(first_day["avgmin2std"])
-    old_asid_newest_message_date = find_day_before_normal_activity(reversed(old_asid_activity_per_day), threshold)
-    new_asid_oldest_message_date = find_day_before_normal_activity(new_asid_activity_per_day, threshold)
+    old_asid_newest_message_date = find_last_entry_below_threshold(reversed(old_asid_activity_per_day), threshold)
+    new_asid_oldest_message_date = find_last_entry_below_threshold(new_asid_activity_per_day, threshold)
 
     cutover_duration = new_asid_oldest_message_date - old_asid_newest_message_date
 
