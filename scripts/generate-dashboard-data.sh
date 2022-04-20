7bf8ea5a1311580df010a1c4df411edd9d74196b
Mark Taylor <86659797+marktaylor80@users.noreply.github.com>
2022-04-20 13:50:33 +0100

    PRACMIG-693: Remove redundant payload from invoke
    This was left over from when the lambda was invoked with some parameters.
    
    Sama / Mark / Caius

--- scripts/generate-dashboard-data.sh
+++ scripts/generate-dashboard-data.sh
@@ -3,5 +3,4 @@
 aws lambda invoke \
   --cli-binary-format raw-in-base64-out \
   --function-name "metrics_calculator" \
-  --payload "{\"oldAsid\": \"493013391039\", \"newAsid\": \"200000032719\", \"odsCode\": \"H84032\"}" \
   metrics_calculator_output.txt
