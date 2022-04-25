c22f5c39800846f50d8127e4a892f2bf62785238
samradnyee.kolas1 <92041477+samradnyee-kolas1@users.noreply.github.com>
2022-04-25 11:42:31 +0100

    PRACMIG-693: Handles splunk query failure
    Sama / Caius

--- metrics-calculator/chalicelib/get_data_from_splunk.py
+++ metrics-calculator/chalicelib/get_data_from_splunk.py
@@ -1,11 +1,16 @@
 import csv
 from http.client import HTTPSConnection
 
+class SplunkQueryError(RuntimeError):
+    pass
+
 
 def get_baseline_threshold_from_splunk_data(asid, baseline_date_range):
     connection = HTTPSConnection("splunk-url")
     connection.request('GET', "/?activationRegion=eu-west-2")
     response = connection.getresponse()
+    if response.status != 200:
+        raise SplunkQueryError(f"Splunk request returned a {response.status} code")
     response_lines = response.read().decode().splitlines()
     csv_reader = csv.DictReader(response_lines)
     first_row = next(csv_reader)
@@ -13,3 +18,4 @@ def get_baseline_threshold_from_splunk_data(asid, baseline_date_range):
     if float(threshold) <= 0:
         raise ValueError("Threshold is not a positive value")
     return threshold
+
--- metrics-calculator/tests/unit/test_get_data_from_splunk.py
+++ metrics-calculator/tests/unit/test_get_data_from_splunk.py
@@ -3,7 +3,7 @@ from unittest.mock import Mock, MagicMock
 
 import pytest as pytest
 
-from chalicelib.get_data_from_splunk import get_baseline_threshold_from_splunk_data
+from chalicelib.get_data_from_splunk import get_baseline_threshold_from_splunk_data, SplunkQueryError
 
 
 @pytest.fixture(scope="function")
@@ -35,5 +35,16 @@ def test_get_baseline_threshold_from_splunk_data_handles_no_results(splunk):
 "2021-09-06T00:00:00.000+0000",0,""0""")
 
     with pytest.raises(ValueError, match="Threshold is not a positive value"):
+        get_baseline_threshold_from_splunk_data(
+            asid, baseline_date_range)
+
+
+def test_get_baseline_threshold_from_splunk_data_handles_HTTP_response_failure(splunk):
+    asid = "12345"
+    baseline_date_range = {"start_date": date(2021, 4, 6), "end_date": date(2021, 6, 28)}
+
+    splunk.return_value = Mock(status=404)
+
+    with pytest.raises(SplunkQueryError, match="Splunk request returned a 404 code"):
         get_baseline_threshold_from_splunk_data(
             asid, baseline_date_range)
