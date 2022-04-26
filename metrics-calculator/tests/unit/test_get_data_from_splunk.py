0fdc85294263c0855f9e41260f53b0f11eabc950
Mark Taylor <86659797+marktaylor80@users.noreply.github.com>
2022-04-26 16:43:29 +0100

    PRACMIG-693: Inline variable in tests
    Inline variables that don't need to be variables since their value is
     irrelevant to the test.

--- metrics-calculator/tests/unit/test_get_data_from_splunk.py
+++ metrics-calculator/tests/unit/test_get_data_from_splunk.py
@@ -29,7 +29,6 @@ def splunk_request(monkeypatch):
 
 
 def test_get_baseline_threshold_from_splunk_data_extracts_threshold_from_splunk_telemetry(splunk_response):
-    asid = "12345"
     baseline_date_range = {"start_date": date(
         2021, 4, 6), "end_date": date(2021, 6, 28)}
 
@@ -37,27 +36,25 @@ def test_get_baseline_threshold_from_splunk_data_extracts_threshold_from_splunk_
 "2021-09-06T00:00:00.000+0000",2,"4537.33933970307""", status=200)
 
     baseline_threshold = get_baseline_threshold_from_splunk_data(
-        "", asid, baseline_date_range)
+        "", "12345", baseline_date_range)
 
     assert baseline_threshold == "4537.33933970307"
 
 
 def test_get_baseline_threshold_from_splunk_data_handles_no_results(splunk_response):
-    asid = "not-a-valid-asid"
     baseline_date_range = {
         "start_date": date(2021, 4, 6),
         "end_date": date(2021, 6, 28)
     }
-
     splunk_response.return_value = Mock(read=lambda: b"""_time",count,avgmin2std
 "2021-09-06T00:00:00.000+0000",0,""0""", status=200)
 
     with pytest.raises(ValueError, match="Threshold is not a positive value"):
-        get_baseline_threshold_from_splunk_data("", asid, baseline_date_range)
+        get_baseline_threshold_from_splunk_data(
+            "", "12345", baseline_date_range)
 
 
 def test_get_baseline_threshold_from_splunk_data_handles_http_response_failure(splunk_response):
-    asid = "12345"
     baseline_date_range = {
         "start_date": date(2021, 4, 6),
         "end_date": date(2021, 6, 28)
@@ -66,11 +63,11 @@ def test_get_baseline_threshold_from_splunk_data_handles_http_response_failure(s
     splunk_response.return_value = Mock(status=404)
 
     with pytest.raises(SplunkQueryError, match="Splunk request returned a 404 code"):
-        get_baseline_threshold_from_splunk_data("", asid, baseline_date_range)
+        get_baseline_threshold_from_splunk_data(
+            "", "12345", baseline_date_range)
 
 
 def test_get_baseline_threshold_from_splunk_data_handles_parse_failure(splunk_response):
-    asid = "12345"
     baseline_date_range = {
         "start_date": date(2021, 4, 6),
         "end_date": date(2021, 6, 28)
@@ -80,7 +77,8 @@ def test_get_baseline_threshold_from_splunk_data_handles_parse_failure(splunk_re
         read=lambda: "this-is-not-a-byte-string", status=200)
 
     with pytest.raises(SplunkParseError):
-        get_baseline_threshold_from_splunk_data("", asid, baseline_date_range)
+        get_baseline_threshold_from_splunk_data(
+            "", "12345", baseline_date_range)
 
 
 def test_get_baseline_threshold_from_splunk_data_makes_correct_request(splunk_request):
@@ -113,7 +111,6 @@ def test_get_baseline_threshold_from_splunk_data_makes_correct_request(splunk_re
 
 
 def test_get_telemetry_from_splunk_get_cutover_telemetry(splunk_response):
-    asid = "12345"
     date_range = {
         "start_date": date(2021, 6, 29),
         "end_date": date(2021, 7, 19)}
@@ -122,13 +119,12 @@ def test_get_telemetry_from_splunk_get_cutover_telemetry(splunk_response):
     splunk_response.return_value = Mock(read=lambda: b"""_time",count,avgmin2std
 "2021-09-06T00:00:00.000+0000",2,"4537.33933970307""", status=200)
 
-    telemetry = get_telemetry_from_splunk("", asid, date_range, threshold)
+    telemetry = get_telemetry_from_splunk("", "12345", date_range, threshold)
 
     assert telemetry == splunk_response.return_value
 
 
 def test_get_telemetry_from_splunk_handles_http_response_failure(splunk_response):
-    asid = "12345"
     date_range = {
         "start_date": date(2021, 6, 29),
         "end_date": date(2021, 7, 19)
@@ -138,7 +134,7 @@ def test_get_telemetry_from_splunk_handles_http_response_failure(splunk_response
     splunk_response.return_value = Mock(status=404)
 
     with pytest.raises(SplunkQueryError, match="Splunk request returned a 404 code"):
-        get_telemetry_from_splunk("", asid, date_range, threshold)
+        get_telemetry_from_splunk("", "12345", date_range, threshold)
 
 
 def test_get_telemetry_from_splunk_has_correct_request_body(splunk_request):
