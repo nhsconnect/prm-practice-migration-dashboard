5cab949058b6aff382414f75982af267de0fc0dc
Mark Taylor <86659797+marktaylor80@users.noreply.github.com>
2022-04-26 16:47:12 +0100

    PRACMIG-693: Create test helper for using an ASID
    The tests don't really care what the value of the ASID being used is, so
     make this clearer by extracting to a function with an appropriate name.

--- metrics-calculator/tests/unit/test_get_data_from_splunk.py
+++ metrics-calculator/tests/unit/test_get_data_from_splunk.py
@@ -36,7 +36,7 @@ def test_get_baseline_threshold_from_splunk_data_extracts_threshold_from_splunk_
 "2021-09-06T00:00:00.000+0000",2,"4537.33933970307""", status=200)
 
     baseline_threshold = get_baseline_threshold_from_splunk_data(
-        "", "12345", baseline_date_range)
+        "", anAsid(), baseline_date_range)
 
     assert baseline_threshold == "4537.33933970307"
 
@@ -51,7 +51,7 @@ def test_get_baseline_threshold_from_splunk_data_handles_no_results(splunk_respo
 
     with pytest.raises(ValueError, match="Threshold is not a positive value"):
         get_baseline_threshold_from_splunk_data(
-            "", "12345", baseline_date_range)
+            "", anAsid(), baseline_date_range)
 
 
 def test_get_baseline_threshold_from_splunk_data_handles_http_response_failure(splunk_response):
@@ -64,7 +64,7 @@ def test_get_baseline_threshold_from_splunk_data_handles_http_response_failure(s
 
     with pytest.raises(SplunkQueryError, match="Splunk request returned a 404 code"):
         get_baseline_threshold_from_splunk_data(
-            "", "12345", baseline_date_range)
+            "", anAsid(), baseline_date_range)
 
 
 def test_get_baseline_threshold_from_splunk_data_handles_parse_failure(splunk_response):
@@ -78,11 +78,11 @@ def test_get_baseline_threshold_from_splunk_data_handles_parse_failure(splunk_re
 
     with pytest.raises(SplunkParseError):
         get_baseline_threshold_from_splunk_data(
-            "", "12345", baseline_date_range)
+            "", anAsid(), baseline_date_range)
 
 
 def test_get_baseline_threshold_from_splunk_data_makes_correct_request(splunk_request):
-    asid = "12345"
+    asid = anAsid()
     baseline_date_range = {
         "start_date": date(2021, 4, 6),
         "end_date": date(2021, 6, 28)
@@ -119,7 +119,7 @@ def test_get_telemetry_from_splunk_get_cutover_telemetry(splunk_response):
     splunk_response.return_value = Mock(read=lambda: b"""_time",count,avgmin2std
 "2021-09-06T00:00:00.000+0000",2,"4537.33933970307""", status=200)
 
-    telemetry = get_telemetry_from_splunk("", "12345", date_range, threshold)
+    telemetry = get_telemetry_from_splunk("", anAsid(), date_range, threshold)
 
     assert telemetry == splunk_response.return_value
 
@@ -134,11 +134,11 @@ def test_get_telemetry_from_splunk_handles_http_response_failure(splunk_response
     splunk_response.return_value = Mock(status=404)
 
     with pytest.raises(SplunkQueryError, match="Splunk request returned a 404 code"):
-        get_telemetry_from_splunk("", "12345", date_range, threshold)
+        get_telemetry_from_splunk("", anAsid(), date_range, threshold)
 
 
 def test_get_telemetry_from_splunk_has_correct_request_body(splunk_request):
-    asid = "12345"
+    asid = anAsid()
     date_range = {
         "start_date": date(2021, 6, 29),
         "end_date": date(2021, 7, 19)
@@ -162,3 +162,7 @@ def test_get_telemetry_from_splunk_has_correct_request_body(splunk_request):
     }
     splunk_request.assert_called_once_with(
         "POST", f"{splunk_base_url}/search/jobs/export", expected_request_body)
+
+
+def anAsid():
+    return "12345"
