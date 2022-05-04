cc53f5cf4b183607c939fec15eb93fbc015c12ec
Mark Taylor <86659797+marktaylor80@users.noreply.github.com>
2022-05-04 16:25:07 +0100

    PRACMIG-693: Ignore Splunk token param changes
    The Splunk API token in Parameter Store is set to a dummy value by the
     Terraform code. It will then be updated manually to its proper, secret,
     value and we don't want Terraform to then replace that with the dummy
     value again when re-applied.
    
    Sama / Mark / Caius

--- dashboard-infra/stacks/metrics-calculator/terraform/ssm.tf
+++ dashboard-infra/stacks/metrics-calculator/terraform/ssm.tf
@@ -4,4 +4,9 @@ resource "aws_ssm_parameter" "splunk_api_token" {
   type        = "SecureString"
   value       = "dummy-api-token-value"
   key_id      = aws_kms_key.splunk_api_token_encryption_key.id
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
 }
